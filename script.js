dd3fd90212deec3264163026787619604bd998cc
tmc2-4 <w2136850@westminster.ac.uk>
2026-03-04 16:37:24 +0000

    fixed search button and header

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    const  searchInput = document.getElementById('movieSearch');
+    const statusDisplay= document.getElementById('searchStatus');
+
+
+    searchInput.oninput = function() {
+
+        if(searchInput.value.length > 0) {
+            statusDisplay.innerText = "Searching for: " + searchInput.value;
+            statusDisplay.style.color ="#f5c518";
+        } else {
+            statusDisplay.innerText = "Awaiting input...";
+            statusDisplay.style.color = "fffff";
+        }
+
+    };
+
+};
