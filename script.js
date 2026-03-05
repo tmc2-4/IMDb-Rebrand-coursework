6e8fc8c8293b65a5599a4deaa1f2ae78bcd3937b
tmc2-4 <w2136850@westminster.ac.uk>
2026-03-05 14:12:50 +0000

    fixed superman poster size

--- script.js
+++ script.js
@@ -13,4 +13,63 @@ window.onload = function() {
             statusDisplay.style.color = "#ffffff";
         }
     };
+
+    const canvas = this.document.getElementById('cinamatic-canvas');
+    const ctx = canvas.getContext('2d')
+    let particlesArray = [];
+
+    function resize() {
+        canvas.width= window.innerWidth;
+        canvas.height = window.innerHeight * 0.7; 
+    }
+
+    window.onresize = resize;
+    resize();
+
+    function init() {
+        for (let i = 0; i < 50; i++) {
+            let particle = {
+                x: Math.random() * canvas.width,
+                y: Math.random() * canvas.height,
+                moveX: (Math.random() - 0.5) * 2,
+                moveY: (Math.random() - 0.5) * 2,
+                size: 2
+            };
+            particlesArray.push(particle)
+        }
+    }
+
+
+    function handleParticles() {
+        for (let i = 0; i < particlesArray.length; i++) {
+            let p = particlesArray[i];
+
+            p.x += p.moveX;
+            p.y += p.moveY
+
+            if (p.x > canvas.width || p.x < 0){
+                p.moveX = p.moveX * -1;
+            }
+
+            if (p.y > canvas.height || p.y < 0){
+                p.moveY = p.moveY * -1;
+            }
+
+            ctx.fillStyle ='#DBA506';
+            ctx.beginPath();
+            ctx.arc(p.x, p.y, p.size, 0, Math.PI *2);
+            ctx.fill();
+        }
+    }
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        handleParticles();
+        requestAnimationFrame(animate);
+    }
+
+    init();
+    animate();
+
+
 };
